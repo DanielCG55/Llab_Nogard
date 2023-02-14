6335443d76e1acb952a539fa526ce4e63b081a0a
Marc Pomar <marc@corecode.school>
2023-02-14 19:44:47 +0100

    fix: changes

--- src/actors/Ammo.ts
+++ src/actors/Ammo.ts
@@ -2,61 +2,73 @@ import { Actor } from "./Actor";
 import { Point } from "../types/Point";
 import { Size } from "../types/Size";
 
-import { distance } from "../utils/distance";
-import { Gohan } from "./Gohan";
-import { checkLimits } from "../utils/checkLimits";
-
 interface InitialAmmoProps {
-    position?: Point;
-    speed?: number;
-    gohan: Gohan;
+  position?: Point;
+  speed?: number;
+  player: Actor;
 }
 
 export class Ammo extends Actor {
-    size: Size;
-    distance?: Point;
-    speed: number;
-    image: HTMLImageElement;
-    gohan: Gohan;
+  size: Size;
+  distance?: Point;
+  speed: number;
+  image: HTMLImageElement;
+  player: Actor;
+  expired: boolean;
+  id: number;
+  constructor(props: InitialAmmoProps) {
+    super(props.position);
+    this.player = props.player;
+    this.id = parseInt((Math.random() * 10000).toFixed(0));
 
-    constructor(props: InitialAmmoProps) {
-        super(props.position);
-        this.gohan = props.gohan;
-        this.position = { x: this.gohan.position.x, y: this.gohan.position.y };
-        this.size = { w: 43, h: 14 };
-        this.speed = 5;
-        this.image = new Image();
-        this.image.src = "weapon.png";
+    this.expired = false;
+
+    // Ammo starts at player postition
+    const { x, y } = this.player.position;
+    this.position = { x, y };
+
+    // Ammo size and speed
+    this.size = { w: 43, h: 14 };
+    this.speed = Math.random() * 10 + 5;
+
+    // Sprite
+    this.image = new Image();
+    this.image.src = "weapon.png";
+  }
+
+  update(delta: number): void {
+    let newPos: Point = {
+      x: this.position.x + this.speed * (delta + 0.5),
+      y: this.position.y,
+    };
+    this.position = newPos;
+
+    // If this bullet is outside canvas, mark it to be deleted
+    if (this.position.x > 1000) {
+      this.expired = true;
     }
+  }
 
-    update(delta: number): void {
-        let newPos: Point = {
-            x: this.position.x + this.speed * (delta + 0.5),
-            y: this.position.y,
-        };
-        this.position = newPos;
-    }
+  draw(ctx: CanvasRenderingContext2D, delta: number): void {
+    ctx.translate(this.position.x, this.position.y);
+    // ctx.fillStyle = "black";
+    // ctx.fillRect(
+    //     -this.size.w / 2,
+    //     -this.size.h / 2,
+    //     this.size.w,
+    //     this.size.h
+    // );
 
-    draw(ctx: CanvasRenderingContext2D, delta: number): void {
-        ctx.translate(this.position.x, this.position.y);
-        // ctx.fillStyle = "black";
-        // ctx.fillRect(
-        //     -this.size.w / 2,
-        //     -this.size.h / 2,
-        //     this.size.w,
-        //     this.size.h
-        // );
-
-        ctx.drawImage(
-            this.image,
-            0,
-            0,
-            43,
-            14,
-            -this.size.w / 2,
-            -this.size.h / 2,
-            this.size.w,
-            this.size.h
-        );
-    }
+    ctx.drawImage(
+      this.image,
+      0,
+      0,
+      43,
+      14,
+      -this.size.w / 2,
+      -this.size.h / 2,
+      this.size.w,
+      this.size.h
+    );
+  }
 }
